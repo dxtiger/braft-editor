82d9c59c889843aad474aebd5b1db3075015570e
tiger <yawei.lu@bhex.io>
2019-12-01 11:21:51 +0800

    链接添加onclick事件

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,65 +1,67 @@
-var path = require('path')
-  , fs = require('fs')
-  , ExtractTextPlugin = require('extract-text-webpack-plugin')
+var path = require("path"),
+  fs = require("fs"),
+  ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   module: {
     //加载器配置
     rules: [
-      { 
+      {
         test: /\.(scss|css)$/,
         use: ExtractTextPlugin.extract([
           // 'style-loader',
-          'css-loader',
-          'sass-loader'
-        ])
-      }, {
+          "css-loader",
+          "sass-loader",
+        ]),
+      },
+      {
         test: /\.(js|jsx)$/,
-        exclude: [
-          /node_modules/,
-          /dist/
-        ],
+        exclude: [/node_modules/, /dist/],
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              ...JSON.parse(fs.readFileSync(path.resolve(__dirname, '../.babelrc'))),
+              ...JSON.parse(
+                fs.readFileSync(path.resolve(__dirname, "../.babelrc")),
+              ),
             },
           },
-          'eslint-loader'
-        ]
-      }, {
+          //'eslint-loader'
+        ],
+      },
+      {
         test: /\.(png|svg)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 8192,
-              name: '[name]_[hash:6].[ext]'
-            }
-          }
-        ]
-      }, {
+              name: "[name]_[hash:6].[ext]",
+            },
+          },
+        ],
+      },
+      {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 20000,
-              name: '[name].[ext]',
-              publicPath: './'
-            }
-          }
-        ]
-      }
-    ]
+              name: "[name].[ext]",
+              publicPath: "./",
+            },
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    modules: [path.resolve(__dirname, '../src'), 'node_modules'],
+    modules: [path.resolve(__dirname, "../src"), "node_modules"],
     alias: {
-      'react': path.join(__dirname, '../node_modules', 'react'),
-      'scssinc': path.join(__dirname, '../src/assets/scss/_inc.scss')
+      react: path.join(__dirname, "../node_modules", "react"),
+      scssinc: path.join(__dirname, "../src/assets/scss/_inc.scss"),
     },
-    extensions: ['.js', '.jsx']
-  }
-}
+    extensions: [".js", ".jsx"],
+  },
+};
--- src/components/business/LinkEditor/index.jsx
+++ src/components/business/LinkEditor/index.jsx
@@ -1,184 +1,219 @@
-import './style.scss'
-import React from 'react'
-import Switch from 'components/common/Switch'
-import DropDown from 'components/common/DropDown'
-import ControlGroup from 'components/business/ControlGroup'
-import { ContentUtils } from 'braft-utils'
+import "./style.scss";
+import React from "react";
+import Switch from "components/common/Switch";
+import DropDown from "components/common/DropDown";
+import ControlGroup from "components/business/ControlGroup";
+import { ContentUtils } from "braft-utils";
 
 export default class LinkEditor extends React.Component {
-
-  constructor (props) {
-
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-      text: '',
-      href: '',
-      target: props.defaultLinkTarget || '',
-      textSelected: false
-    }
-
+      text: "",
+      href: "",
+      target: props.defaultLinkTarget || "",
+      textSelected: false,
+    };
   }
 
-  dropDownInstance = null
+  dropDownInstance = null;
 
-  componentWillReceiveProps (nextProps) {
+  componentWillReceiveProps(nextProps) {
+    const { href, target } = ContentUtils.getSelectionEntityData(
+      nextProps.editorState,
+      "LINK",
+    );
+    const textSelected =
+      !ContentUtils.isSelectionCollapsed(this.props.editorState) &&
+      ContentUtils.getSelectionBlockType(this.props.editorState) !== "atomic";
 
-    const { href, target } = ContentUtils.getSelectionEntityData(nextProps.editorState, 'LINK')
-    const textSelected = !ContentUtils.isSelectionCollapsed(this.props.editorState) && ContentUtils.getSelectionBlockType(this.props.editorState) !== 'atomic'
-
-    let selectedText = ''
+    let selectedText = "";
 
     if (textSelected) {
-      selectedText = ContentUtils.getSelectionText(this.props.editorState)
+      selectedText = ContentUtils.getSelectionText(this.props.editorState);
     }
 
     this.setState({
       textSelected: textSelected,
       text: selectedText,
-      href: href || '',
-      target: typeof target === 'undefined' ? (nextProps.defaultLinkTarget || '') : (target || '')
-    })
-
+      href: href || "",
+      target:
+        typeof target === "undefined"
+          ? nextProps.defaultLinkTarget || ""
+          : target || "",
+    });
   }
 
-  render () {
-
-    const { allowInsertLinkText } = this.props
-    const { text, href, target, textSelected } = this.state
-    const caption = <i className='bfi-link'></i>
+  render() {
+    const { allowInsertLinkText } = this.props;
+    const { text, href, target, textSelected } = this.state;
+    const caption = <i className="bfi-link"></i>;
+    const hookReturns = this.props.hooks("open-link");
 
     return (
       <ControlGroup>
         <DropDown
           key={0}
           caption={caption}
+          onClick={() => {
+            hookReturns && hookReturns();
+          }}
           title={this.props.language.controls.link}
           autoHide={true}
           getContainerNode={this.props.getContainerNode}
           showArrow={false}
-          ref={(instance) => this.dropDownInstance = instance}
-          className={'control-item dropdown link-editor-dropdown'}
+          ref={(instance) => (this.dropDownInstance = instance)}
+          className={"control-item dropdown link-editor-dropdown"}
         >
-          <div className='bf-link-editor'>
-            {allowInsertLinkText ? <div className='input-group'>
+          <div className="bf-link-editor">
+            {allowInsertLinkText ? (
+              <div className="input-group">
+                <input
+                  type="text"
+                  value={text}
+                  spellCheck={false}
+                  disabled={textSelected}
+                  placeholder={
+                    this.props.language.linkEditor.textInputPlaceHolder
+                  }
+                  onKeyDown={this.handeKeyDown}
+                  onChange={this.handleTnputText}
+                />
+              </div>
+            ) : null}
+            <div className="input-group">
               <input
-                type='text'
-                value={text}
-                spellCheck={false}
-                disabled={textSelected}
-                placeholder={this.props.language.linkEditor.textInputPlaceHolder}
-                onKeyDown={this.handeKeyDown}
-                onChange={this.handleTnputText}
-              />
-            </div> : null}
-            <div className='input-group'>
-              <input
-                type='text'
+                type="text"
                 value={href}
                 spellCheck={false}
-                placeholder={this.props.language.linkEditor.linkInputPlaceHolder}
+                placeholder={
+                  this.props.language.linkEditor.linkInputPlaceHolder
+                }
                 onKeyDown={this.handeKeyDown}
                 onChange={this.handleInputLink}
               />
             </div>
-            <div className='switch-group'>
-              <Switch
-                active={target === '_blank'}
-                onClick={this.setTarget}
-              />
+            <div className="switch-group">
+              <Switch active={target === "_blank"} onClick={this.setTarget} />
               <label>{this.props.language.linkEditor.openInNewWindow}</label>
             </div>
-            <div className='buttons'>
-              <a onClick={this.handleUnlink} className='primary button-remove-link pull-left'>
-                <i className='bfi-close'></i>
+            <div className="buttons">
+              <a
+                onClick={this.handleUnlink}
+                className="primary button-remove-link pull-left"
+              >
+                <i className="bfi-close"></i>
                 <span>{this.props.language.linkEditor.removeLink}</span>
               </a>
-              <button type='button' onClick={this.handleConfirm} className='primary pull-right'>{this.props.language.base.confirm}</button>
-              <button type='button' onClick={this.handleCancel} className='default pull-right'>{this.props.language.base.cancel}</button>
+              <button
+                type="button"
+                onClick={this.handleConfirm}
+                className="primary pull-right"
+              >
+                {this.props.language.base.confirm}
+              </button>
+              <button
+                type="button"
+                onClick={this.handleCancel}
+                className="default pull-right"
+              >
+                {this.props.language.base.cancel}
+              </button>
             </div>
           </div>
         </DropDown>
         <button
           key={1}
-          type='button'
+          type="button"
           data-title={this.props.language.controls.unlink}
-          className='control-item button'
+          className="control-item button"
           onClick={this.handleUnlink}
           disabled={!textSelected || !href}
         >
-          <i className='bfi-link-off'></i>
+          <i className="bfi-link-off"></i>
         </button>
       </ControlGroup>
-    )
-
+    );
   }
 
   handeKeyDown = (e) => {
     if (e.keyCode === 13) {
-      this.handleConfirm()
-      e.preventDefault()
-      return false
+      this.handleConfirm();
+      e.preventDefault();
+      return false;
     }
-  }
+  };
 
   handleTnputText = (e) => {
     this.setState({
-      text: e.currentTarget.value
-    })
-  }
+      text: e.currentTarget.value,
+    });
+  };
 
   handleInputLink = (e) => {
     this.setState({
-      href: e.currentTarget.value
-    })
-  }
+      href: e.currentTarget.value,
+    });
+  };
 
   setTarget = () => {
     this.setState({
-      target: this.state.target === '_blank' ? '' : '_blank'
-    })
-  }
+      target: this.state.target === "_blank" ? "" : "_blank",
+    });
+  };
 
   handleCancel = () => {
-    this.dropDownInstance.hide()
-  }
+    this.dropDownInstance.hide();
+  };
 
   handleUnlink = () => {
-    this.dropDownInstance.hide()
-    this.props.editor.setValue(ContentUtils.toggleSelectionLink(this.props.editorState, false))
-  }
+    this.dropDownInstance.hide();
+    this.props.editor.setValue(
+      ContentUtils.toggleSelectionLink(this.props.editorState, false),
+    );
+  };
 
   handleConfirm = () => {
+    let { text, href, target, textSelected } = this.state;
+    const hookReturns = this.props.hooks("toggle-link", { href, target })({
+      href,
+      target,
+    });
 
-    let { text, href, target, textSelected } = this.state
-    const hookReturns = this.props.hooks('toggle-link', { href, target })({ href, target })
-
-    this.dropDownInstance.hide()
-    this.props.editor.requestFocus()
+    this.dropDownInstance.hide();
+    this.props.editor.requestFocus();
 
     if (hookReturns === false) {
-      return false
+      return false;
     }
 
     if (hookReturns) {
-      typeof hookReturns.href === 'string' && (href = hookReturns.href)
-      typeof hookReturns.target === 'string' && (target = hookReturns.target)
+      typeof hookReturns.href === "string" && (href = hookReturns.href);
+      typeof hookReturns.target === "string" && (target = hookReturns.target);
     }
 
     if (textSelected) {
       if (href) {
-        this.props.editor.setValue(ContentUtils.toggleSelectionLink(this.props.editorState, href, target))
+        this.props.editor.setValue(
+          ContentUtils.toggleSelectionLink(
+            this.props.editorState,
+            href,
+            target,
+          ),
+        );
       } else {
-        this.props.editor.setValue(ContentUtils.toggleSelectionLink(this.props.editorState, false))
+        this.props.editor.setValue(
+          ContentUtils.toggleSelectionLink(this.props.editorState, false),
+        );
       }
     } else {
-      this.props.editor.setValue(ContentUtils.insertText(this.props.editorState, text || href, null, {
-        type: 'LINK',
-        data: { href, target }
-      }))
+      this.props.editor.setValue(
+        ContentUtils.insertText(this.props.editorState, text || href, null, {
+          type: "LINK",
+          data: { href, target },
+        }),
+      );
     }
-
-  }
-
+  };
 }
